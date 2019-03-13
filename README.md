7cc31ffe8005e512a1df30c5c2b5f5f0cce580ed
pioc92 <julien.darribau@gmail.com>
2019-03-13 15:39:05 +0100

    check amazon's shortcut urls
    Check if the link posted by user is an amazon's shorcut url and change it to a classic amazon's url to find the affiliate tag

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Works with advanced editor only
 Automatically add your amazon's tag to amazon's user posts
 Edit class.amazontag.plugin.php and :
--add your amazon associates website (amazon.fr, amazon.com, etc) on line 31
--replace YOUR AMAZON TAG by yours (example mysite-21) on line 33
+-add your amazon associates website (amazon.fr, amazon.com, etc) on line 32
+-replace YOUR AMAZON TAG by yours (example mysite-21) on line 40
 
-
+version 0.3 : Check shortcut amazon's links to find user's affiliates tags instead of yours
 version 0.2 : replace original url by the tagged one. I forgot it in the initial release (i'm idiot)
 version 0.1 initial release
--- class.amazontag.plugin.php
+++ class.amazontag.plugin.php
@@ -29,9 +29,15 @@ class AmazontagPlugin extends Gdn_Plugin {
                 //Extract and show the "href" attribute.
                 $url =  $link->getAttribute('href');
                 $url2 = $link->getAttribute('href');
-                 $checkdomain = 'amazon.fr'; //your amazon associates website
+                $checkdomain = 'amazon.fr'; //your amazon associates website to modify if you're not french!
+                if (!empty($url) && strpos($url, 'https://amzn.to') !== false){ //if link is an amazon https shortcut url, we find the final's amazon url (ex https://amzn.to/slfdol -> https://www.amazon.com/product/tag=example21)
+                    $url = $this->get_redirect_target($url); 
+                }
+                if (!empty($url) && strpos($url, 'http://amzn.to') !== false){ //if link is an amazon http shortcut url, we find the final's amazon url (http://amzn.to-> https://amzn.to-> https://www.amazon.com/product/tag=example21)
+                    $url = $this->get_redirect_final_target($url); 
+                }
                 if (!empty($url) && strpos($url, $checkdomain) !== false){
-                    $afftag = 'YOUR AFFILIATE TAG'; //our affiliate ID
+                    $afftag = 'YOUR AFFILIATE TAG'; //your affiliate ID to modify
                     $affstring = 'tag='; // url parameter for affiliate ID
 
                     if (parse_url($url, PHP_URL_QUERY)){ //check if link has query string
@@ -53,5 +59,36 @@ class AmazontagPlugin extends Gdn_Plugin {
                 }
             }
         }
+    }
+    ///
+    /// https://gist.github.com/davejamesmiller/dbefa0ff167cc5c08d6d code to find redirected url
+    ///
+    public function get_redirect_target($url) //function to find redirected url
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $headers = curl_exec($ch);
+        curl_close($ch);
+        // Check if there's a Location: header (redirect)
+        if (preg_match('/^Location: (.+)$/im', $headers, $matches))
+            return trim($matches[1]);
+        // If not, there was no redirect so return the original URL
+        // (Alternatively change this to return false)
+        return $url;
+    }
+    public function get_redirect_final_target($url) //function to find final redirected url
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // follow redirects
+        curl_setopt($ch, CURLOPT_AUTOREFERER, 1); // set referer on redirect
+        curl_exec($ch);
+        $target = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+        curl_close($ch);
+        if ($target)
+            return $target;
+        return false;
     }
 }
